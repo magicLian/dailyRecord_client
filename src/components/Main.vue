39e577c2bddf68e2a960541e3b3a0b80b93c1d32
magicLian <821579754@qq.com>
2018-04-13 16:01:57 +0800

    new

---
+++ src/components/Main.vue
@@ -0,0 +1,37 @@
+<template>
+    <el-container>
+        <el-header>{{header}}</el-header>
+        <el-main></el-main>
+    </el-container>
+</template>
+
+<script>
+export default {
+    name: 'mainPage',
+    data () {
+        return {
+	        header: 'Welcome to Your Vue.js App',
+            body :"body"
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .el-header, .el-footer {
+        background-color: #B3C0D1;
+        color: #333;
+        text-align: center;
+        line-height: 60px;
+    }
+    .el-main {
+        background-color: #E9EEF3;
+        color: #333;
+        text-align: center;
+        line-height: 160px;
+    }
+
+    body > .el-container {
+        margin-bottom: 40px;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue';
+import Router from 'vue-router';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import MainPage from '@/components//Main';
 
-Vue.use(Router)
+Vue.use(Router);
+Vue.use(ElementUI);
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+	routes: [{
+		path: '/',
+		name: 'mainPage',
+		component: MainPage
+	}]
 })
