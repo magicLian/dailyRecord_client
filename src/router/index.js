008c51d01740c0c4316fb4ed8ec01747f74e01ae
magicLian <821579754@qq.com>
2018-04-14 15:38:34 +0800

    Revert "new"
    This reverts commit 39e577c

--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,15 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import ElementUI from 'element-ui';
-import '@/assets/css/cssReset.css';
-import 'element-ui/lib/theme-chalk/index.css';
-import MainPage from '@/components/main';
-import SearchFood from '@/components/main/searchFood';
-import Home from '@/components/main/home';
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
 
-Vue.use(Router);
-Vue.use(ElementUI);
+Vue.use(Router)
 
 export default new Router({
-	routes: [
-		{
-			path: '/',
-			redirect: '/index'
-		},
-		{
-			path : '/index',
-			redirect: '/index/home',
-			name: 'mainPage',
-			component: MainPage,
-				children:[
-				{
-					path: '/index/project/searchFood',
-					name:'SearchFood',
-					component:SearchFood
-				},
-				{
-					path: '/index/home',
-					name:'Home',
-					component:Home,
-				}]
-		}
-	]
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
+  ]
 })
